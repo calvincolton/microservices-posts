fbd0f1c9a748ee18bf1173862397904e7ffb1d13
Calvin Colton <calvincolton@gmail.com>
2020-07-30 13:41:03 -0700

    boilerplate, pre dedicated microservice for comments

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentsId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentsId, content});
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+const PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`Listending on port ${PORT}`);
+})
